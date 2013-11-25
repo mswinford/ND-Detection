68740b0e1aa361d2a4f0db24bcf22cc057598816
Zhe Cao <cao43@sslab19.cs.purdue.edu>
2013-11-24 20:14:45 -0500

    change the format of dataset

--- QueryFile.java
+++ QueryFile.java
@@ -7,7 +7,6 @@ static String content =null;
 static String topic = null;
 static List<String> lines = null;
 final static String FILEDIRECTORY = "./data"; 
-
 static String outputfile;
 
 private static String readFile( File file ) throws IOException {
@@ -17,29 +16,27 @@ private static String readFile( File file ) throws IOException {
     String         ls = System.getProperty("line.separator");
 
     while( ( line = reader.readLine() ) != null ) {
-      Scanner tokenizer = new Scanner(line);  
-     String word1 = tokenizer.next(); 
-     if(word1.equals("topic")){
-     topic = line;
-     }
-     if(word1.equals("comment")){
-        StringBuffer s = new StringBuffer(line);
- StringBuffer removed =s.delete(0,10);    
-        line = removed.toString();   
-
-       stringBuilder.append( line );
-       //System.out.println("found");
-     break;
-     }
+    	Scanner tokenizer = new Scanner(line);  
+     	String word1 = tokenizer.next(); 
+     	if(word1.equals("topic")){
+     		topic = line;
+     	}
+     	if(word1.equals("comment")){
+        	StringBuffer s = new StringBuffer(line);
+ 		StringBuffer removed =s.delete(0,10);    
+        	line = removed.toString();   
+		stringBuilder.append( line );
+      		//System.out.println("found");
+     		break;
+    	 }
      
      
-      //stringBuilder.append( line );
-        //stringBuilder.append( ls );
+      		//stringBuilder.append( line );
+        	//stringBuilder.append( ls );
     }
 
     while( ( line = reader.readLine() ) != null ) {
-      
-      stringBuilder.append( line );
+    	stringBuilder.append( line );
         //stringBuilder.append( ls );
     }
     
@@ -48,39 +45,39 @@ private static String readFile( File file ) throws IOException {
 }
 
 
-  public static void listFilesForFolder( File folder) {
-    File out = new File(outputfile);
-     try{
-         PrintWriter clean = new PrintWriter(out);
-         clean.println("");
+public static void listFilesForFolder( File folder) {
+	File out = new File(outputfile);
+	try{
+        	PrintWriter clean = new PrintWriter(out);
+         	clean.println("");
         }catch (Exception e){//Catch exception if any
-  System.err.println("Error: " + e.getMessage());
-  }
-    for (final File fileEntry : folder.listFiles()) {
-      //System.out.println(fileEntry.getName());
-      if(lines.contains(fileEntry.getName())){
-        if (fileEntry.isDirectory()) {
-            listFilesForFolder(fileEntry);
-        } else {
-            //System.out.println(fileEntry.getName());
-           String fileName = fileEntry.getName(); 
-    File parentDir = new File(FILEDIRECTORY); // New file (parent file ..)   
-    File newFile = new File(parentDir,fileName); //open the file
+  		System.err.println("Error: " + e.getMessage());
+  	}
+   	 for (final File fileEntry : folder.listFiles()) {
+      		//System.out.println(fileEntry.getName());
+      	if(lines.contains(fileEntry.getName())){
+        	if (fileEntry.isDirectory()) {
+            	listFilesForFolder(fileEntry);
+       		 } else {
+            		//System.out.println(fileEntry.getName());
+          	 String fileName = fileEntry.getName(); 
+    		 File parentDir = new File(FILEDIRECTORY); // New file (parent file ..)   
+    		 File newFile = new File(parentDir,fileName); //open the file
               try{
-      //System.out.println("here");
-   content=readFile(newFile);
-    }catch(Exception e){
-    //do nothing
-    }         
-             try{
-  // Create file 
-  PrintWriter write = new PrintWriter(new FileWriter(out, true));
+      		//System.out.println("here");
+   		content=readFile(newFile);
+    		}catch(Exception e){
+    		//do nothing
+   		 }         
+               try{
+  		// Create file 
+ 		 PrintWriter write = new PrintWriter(new FileWriter(out, true));
 
-   write.println("<DOC>");
-  write.println("<DOCNO>"+fileEntry.getName()+"</DOCNO>");
-    write.println("<TEXT>");
-                  //write.println("");
-    Scanner sc = new Scanner(content);  
+   		write.println("<DOC>");
+ 		 write.println("<DOCNO>"+fileEntry.getName()+"</DOCNO>");
+    		write.println("<TEXT>");
+                  write.println(content);
+    /*Scanner sc = new Scanner(content);  
      while( sc.hasNext() ) {
       
      String word = sc.next(); 
@@ -89,7 +86,7 @@ private static String readFile( File file ) throws IOException {
      }
      //System.out.println(word);
                     write.println(word);
-     }
+     }*/
                     write.println("</TEXT>");
                       write.println("</DOC>");
   //Close the output stream
